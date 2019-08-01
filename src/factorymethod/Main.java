bbf71bdafc8aa0b82a60b12676c668897f3fa409
Pedro Almeida <pedro.almei@hotmail.com>
2019-08-01 16:55:04 -0300

    First commit

---
+++ src/factorymethod/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorymethod;
+
+/**
+ *
+ * @author alzarus
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        ICreator criadorSapato = new CreatorSapato();
+        ICreator criadorVentilador = new CreatorVentilador();
+        criadorSapato.create();
+        criadorVentilador.create();
+    }
+}
